19487fe418c1fa5a85aa33d545f0081b24551b13
Aymen Djellal <aymendjellal@gmail.com>
2017-04-10 12:59:26 -0400

    Wrong test.json

--- axonsegpy/algo/AxonSeg.py
+++ axonsegpy/algo/AxonSeg.py
@@ -94,7 +94,7 @@ def run(params):
     except KeyError:
         f_output = f_input + ".list.bin"
     image = io.imread(f_input, as_grey=True)
-    axonList=axonSeg(image,{"minSize":30,"Solidity":0.3,"MinorMajorRatio":0.1})
+    axonList=axonSeg(image,params)
     axonList.save(f_output)
 
 
